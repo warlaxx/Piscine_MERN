b15bd956368f7e4d8b62aa79fef83d752d8255c5
Audrey Claris <audrey claris>
2020-05-05 18:19:33 +0200

    Day 2 - PM

--- Jour_02/ex_06/server.js
+++ Jour_02/ex_06/server.js
@@ -1,36 +1,21 @@
 const express = require('express')
 const app = express()
 fs = require('fs');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-
 var MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017/mern-pool', { useUnifiedTopology: true }, function(err, client) {
     if (err) { console.log("Connection failed.") } else { console.log("Connection Successfull") };
 
     var db = client.db('mern-pool');
-    // db.collection('students').findOne({}, function(findErr, result) {
-    // if (findErr) throw findErr;
-    //     if (result) {
-    //         console.log("oups");
-    //     } else {
-    //         console.log("no");
-    //     }
-    //     // console.log(result.firstname);
-
-
-    //     client.close();
-    // });
-
     app.get('/', function(req, res) {
-        res.write("wesh");
+        db.collection("students").find({ validated: "in progress" }).sort({ lastname: 1 }).toArray(function(err, result) {
+            if (err) throw err;
+            res.json(result)
+            client.close();
+        });
     })
-
-
 });
-
-
 
 app.listen(4242);
